9e05ac0faa2d8f303181007b5231f3a7fbac3894
AndresFelipeCasasLizarazo <andresfelipecasaslizarazo@gmail.com>
2025-08-22 14:17:26 -0500

    feat: añadir test de ejemplo

---
+++ test/test_ejemplo.py
@@ -0,0 +1,11 @@
+# tests/test_ejemplo.py
+
+
+def test_suma_dos_numeros():
+    """Verifica que la suma de 2 + 2 es 4."""
+    assert 2 + 2 == 4
+
+
+def test_cadena_no_vacia():
+    """Verifica que una cadena no está vacía."""
+    assert "Hola Mundo" != ""
